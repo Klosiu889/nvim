330a29c3c64ce9df5867a0879adb36db68eddcc3
Jakub Kłos <93655086+Klosiu889@users.noreply.github.com>
2024-03-21 00:34:03 +0100

    Simplifying feline fix

--- lua/klosiu/plugins/feline.lua
+++ lua/klosiu/plugins/feline.lua
@@ -2,22 +2,11 @@ return {
     "feline-nvim/feline.nvim",
     config = function()
         local C = require("catppuccin.palettes").get_palette()
-        local lsp = require("feline.providers.lsp")
         local ctp_feline = require("catppuccin.groups.integrations.feline")
 
         -- Making feline consistent with diagnostics colors
         local components = ctp_feline.get()
-        components.active[2][5] = {
-            provider = "diagnostic_hints",
-            enabled = function()
-                return lsp.diagnostics_exist(vim.diagnostic.severity.HINT)
-            end,
-            hl = {
-                fg = C.teal, -- only change, previously rosewater
-                bg = C.mantle,
-            },
-            icon = " " .. "" .. " ",
-        }
+        components.active[2][5].hl.fg = C.teal
 
         require("feline").setup({
             components = components,
