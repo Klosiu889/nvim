5f92eed3a41cee358af61dacdb63155ffbf8d15d
Jakub Kłos <jakubklos02@gmail.com>
2025-06-30 02:54:48 +0200

    refactored lualine

--- lua/klosiu/plugins/lualine.lua
+++ lua/klosiu/plugins/lualine.lua
@@ -4,9 +4,85 @@ return {
 
     config = function()
         local lualine = require("lualine")
-        local C = require("catppuccin.palettes").get_palette()
-        local O = require("catppuccin").options
-        local custom = {}
+        local colors = require("catppuccin.palettes").get_palette()
+        local options = require("catppuccin").options
+        local transparent_bg = options.transparent_background and "NONE" or colors.mantle
+
+        local theme = {
+            normal = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            insert = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            visual = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            replace = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            command = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            inactive = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+        }
+
+        local mode_color = {
+            n = colors.lavender,
+            i = colors.green,
+            v = colors.flamingo,
+            [""] = colors.flamingo,
+            V = colors.flamingo,
+            c = colors.peach,
+            no = colors.red,
+            s = colors.yellow,
+            S = colors.yellow,
+            [""] = colors.yellow,
+            ic = colors.peach,
+            R = colors.maroon,
+            Rv = colors.maroon,
+            cv = colors.red,
+            ce = colors.red,
+            r = colors.blue,
+            rm = colors.blue,
+            ["r?"] = colors.blue,
+            ["!"] = colors.red,
+            t = colors.teal,
+        }
+
+        local transparent_component = { bg = transparent_bg, fg = colors.overlay1 }
 
         local function show_macro_recording()
             local recording_register = vim.fn.reg_recording()
@@ -17,141 +93,108 @@ return {
             end
         end
 
-        local transparent_bg = O.transparent_background and "NONE" or C.mantle
-        custom.normal = {
-            a = { bg = C.lavender, fg = C.mantle, gui = "bold" },
-            b = { bg = C.surface0, fg = C.blue },
-            c = { bg = transparent_bg, fg = C.text },
+        local function get_cwd()
+            return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+        end
+
+        local mode = {
+            "mode",
+            icon = "",
+            color = function()
+                return { bg = mode_color[vim.fn.mode()], fg = colors.base }
+            end,
+            separator = { left = "", right = "" },
         }
 
-        custom.insert = {
-
-            a = { bg = C.green, fg = C.base, gui = "bold" },
-            b = { bg = C.surface0, fg = C.green },
+        local diff = {
+            "diff",
+            color = { bg = colors.mauve, fg = colors.base, gui = "bold" },
+            separator = { right = "" },
+            symbols = { added = " ", modified = " ", removed = " " },
+            colored = false,
         }
 
-        custom.terminal = {
-            a = { bg = C.green, fg = C.base, gui = "bold" },
-            b = { bg = C.surface0, fg = C.green },
+        local progress = {
+            "progress",
+            color = transparent_component,
         }
 
-        custom.command = {
-            a = { bg = C.peach, fg = C.base, gui = "bold" },
-            b = { bg = C.surface0, fg = C.peach },
+        local location = {
+            "location",
+            color = transparent_component,
         }
 
-        custom.visual = {
-            a = { bg = C.flamingo, fg = C.base, gui = "bold" },
-            b = { bg = C.surface0, fg = C.mauve },
+        local searchcount = {
+            "searchcount",
+            color = transparent_component,
         }
 
-        custom.replace = {
-            a = { bg = C.maroon, fg = C.base, gui = "bold" },
-            b = { bg = C.surface0, fg = C.red },
+        local macro_recording = {
+            show_macro_recording,
+            color = transparent_component,
         }
 
-        custom.inactive = {
-            a = { bg = transparent_bg, fg = C.blue },
-            b = { bg = transparent_bg, fg = C.surface1, gui = "bold" },
-            c = { bg = transparent_bg, fg = C.overlay0 },
+        local sep = {
+            "%=",
+            color = transparent_component,
         }
 
-        local transparent_component = { bg = transparent_bg, fg = C.overlay1 }
+        local diagnostics = {
+            "diagnostics",
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+            colored = true,
+        }
 
+        local branch = {
+            "branch",
+            icon = "",
+            color = transparent_component,
+        }
+
+        local lsp = {
+            "lsp_status",
+            icon = "󰅡",
+            color = transparent_component,
+        }
+        local filetype = {
+            "filetype",
+            color = { bg = colors.maroon, fg = colors.base, gui = "bold" },
+            padding = { left = 1, right = 0 },
+            colored = false,
+            icon_only = true,
+        }
+        local filename = {
+            "filename",
+            color = { bg = colors.maroon, fg = colors.base, gui = "bold" },
+            padding = { left = 0, right = 1 },
+            file_status = false,
+        }
+        local cwd = {
+            get_cwd,
+            icon = "󰉖",
+            color = { bg = colors.flamingo, fg = colors.base, gui = "bold" },
+            separator = { left = "", right = "" },
+        }
         lualine.setup({
             options = {
-                theme = custom,
+                theme = theme,
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
                 always_divide_middle = false,
             },
             sections = {
-                lualine_a = {
-                    {
-                        "mode",
-                        icon = "",
-                        separator = { left = "", right = "" },
-                    },
-                },
-                lualine_b = {
-                    {
-                        "diff",
-                        color = { bg = C.mauve, fg = C.base, gui = "bold" },
-                        separator = { right = "" },
-                        symbols = { added = " ", modified = " ", removed = " " },
-                        colored = false,
-                    },
-                },
-                lualine_c = {
-                    {
-                        "progress",
-                        color = transparent_component,
-                    },
-                    {
-                        "location",
-                        color = transparent_component,
-                    },
-                    {
-                        "searchcount",
-                        color = transparent_component,
-                    },
-                    {
-                        show_macro_recording,
-                        color = transparent_component,
-                    },
-                    {
-                        "%=",
-                    },
-                    {
-                        "diagnostics",
-                        symbols = { error = " ", warn = " ", info = " ", hint = " " },
-                        colored = true,
-                    },
-                },
-                lualine_x = {
-                    {
-                        "branch",
-                        icon = "",
-                        color = transparent_component,
-                    },
-                    {
-                        "lsp_status",
-                        icon = "󰅡",
-                        color = transparent_component,
-                    },
-                },
-                lualine_y = {
-                    {
-                        "filetype",
-                        color = { bg = C.maroon, fg = C.base, gui = "bold" },
-                        padding = { left = 1, right = 0 },
-                        colored = false,
-                        icon_only = true,
-                    },
-                    {
-                        "filename",
-                        color = { bg = C.maroon, fg = C.base, gui = "bold" },
-                        padding = { left = 0, right = 1 },
-                        file_status = false,
-                    },
-                },
-                lualine_z = {
-                    {
-                        "filename",
-                        icon = "󰉖",
-                        color = { bg = C.flamingo, fg = C.base, gui = "bold" },
-                        separator = { left = "", right = "" },
-                        fmt = function(_)
-                            return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-                        end,
-                    },
-                },
+                lualine_a = { mode },
+                lualine_b = { diff },
+                lualine_c = { progress, location, searchcount, macro_recording, sep, diagnostics },
+                lualine_x = { branch, lsp },
+                lualine_y = { filetype, filename },
+                lualine_z = { cwd },
             },
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "location" },
+                lualine_c = {},
+                lualine_x = {},
                 lualine_y = {},
                 lualine_z = {},
             },
